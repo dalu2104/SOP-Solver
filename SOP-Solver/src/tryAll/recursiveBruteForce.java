7a8d437604050b7fe8b037703cb457651aa8017b
Thore <thorepaetsch@web.de>
2017-11-14 18:10:10 +0100

    DIM gibt jetzt tatsächlich die Dimension an, Start- und Endknoten werden erstmal hinzugefügt, valid daher erweitert

--- SOP-Solver/src/tryAll/recursiveBruteForce.java
+++ SOP-Solver/src/tryAll/recursiveBruteForce.java
@@ -27,7 +27,7 @@ public class recursiveBruteForce {
 	public static List<Integer> perfectResult(int[][] weights){
 		matrix = weights;
 		//the dimension without the destination
-		DIM = weights[0].length -1;
+		DIM = weights[0].length;
 		bestPath = new ArrayList<Integer>();
 		bestLength = Integer.MAX_VALUE;
 		List<Integer> curPath = new ArrayList<Integer>();
@@ -51,7 +51,8 @@ public class recursiveBruteForce {
 	 * 			the length the current solution path has so far.
 	 */
 	private static void recursion(List<Integer> curPath, int curLength){
-		for(int i=1; i<DIM; i++){
+		System.out.println(curPath.toString());///////////////////////////////////////print-line-debugging
+		for(int i=0; i<DIM; i++){
 			if(validPath(i, curPath)){
 				if(!curPath.isEmpty()){
 					curLength += matrix[curPath.get(curPath.size()-1)][i];
@@ -59,7 +60,7 @@ public class recursiveBruteForce {
 				curPath.add(i);
 				//checking if the solution-Path is complete.
 				//	it is complete with DIM-1 nodes (because starting point and destination have to miss)
-				if(curPath.size()<DIM-1){
+				if(curPath.size()<DIM){
 					//cur-Path is not a complete solution-Path
 					recursion(curPath, curLength);
 				} else {
@@ -70,6 +71,7 @@ public class recursiveBruteForce {
 						bestPath = curPath;
 					}
 				}
+				curPath.remove(curPath.size()-1);
 			}
 		}
 	}
@@ -79,6 +81,7 @@ public class recursiveBruteForce {
 	 * Contains checking if the node is not allready in the path and if all dependencies are fullfilled after adding it.
 	 * All dependencies for the node are fullfilled, if every node that has to be visited before the node to check are allready in the path.
 	 * If node i has to be visited before the node, matrix[node][i] is -1.
+	 * Also ensures, that the first node is always the start of a path and the last node always the destination.
 	 * 
 	 * @param node
 	 * 			the given node that is eventually added to the solution-path.
@@ -88,17 +91,38 @@ public class recursiveBruteForce {
 	 */
 	private static boolean validPath(int node, List<Integer> curPath){
 		boolean result = true;
-		//checking if the node is allready in the path
-		if(curPath.contains(node)){
-			result = false;
-		} else {
-			//checking the dependencies
-			for(int i=1; i<DIM; i++){
-				if(matrix[node][i]==-1){
-					if(!curPath.contains(i)){
-						result=false;
+		if(!curPath.isEmpty()){
+			if(curPath.size()==DIM-1){
+				//only the destination is missing
+				if(node != DIM-1){
+					result = false;
+				}
+			} else {
+				//there are still more nodes than the destination missing
+				if(node == DIM-1){
+					//we don't want the destination node if we don't have all the other nodes in the path
+					result = false;
+				} else if(node == 0){
+					//we don't want the starting point unless curPath is empty
+					result = false;
+				} else if(curPath.contains(node)){
+					//checking if the node is allready in the path
+					result = false;
+				} else {
+					//checking the dependencies
+					for(int i=0; i<DIM; i++){
+						if(matrix[node][i]==-1){
+							if(!curPath.contains(i)){
+								result=false;
+							}
+						}
 					}
 				}
+			}
+		} else {
+			//curPath is empty, only the starting point is allowed
+			if(node != 0){
+				result = false;
 			}
 		}
 		return result;
