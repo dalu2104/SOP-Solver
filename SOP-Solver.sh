d9c8923aa0e8e9afae947458681c4ceb911ba83c
dalu2104 <stu124145@mail.uni-kiel.de>
2017-11-20 14:11:37 +0100

    Fixed minor bugs from transition to Linux.

--- SOP-Solver.sh
+++ SOP-Solver.sh
@@ -3,5 +3,3 @@ echo Willkommen beim SOP-Solver.
 echo Waehlen Sie bitte eine Testinstanz aus dem Ordner 'SOP-Instanzen':
 read -p "Dateiname inkl. Dateiendung, zB esc07.sop:" dateiname
 java -cp build execution.Exe $PWD/SOP-Instanzen/$dateiname
-echo Zum Beenden:
-PAUSE
