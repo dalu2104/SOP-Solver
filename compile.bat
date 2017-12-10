a3fb55d98bc0ea167cb3be287d15f2329df48dee
dalu2104 <stu124145@mail.uni-kiel.de>
2017-12-10 13:59:00 +0100

    Updated build scripts for SA algorithm.

--- compile.bat
+++ compile.bat
@@ -5,5 +5,6 @@ javac -d build "%~dp0\SOP-Solver\src\validSolution\OneSolution.java"
 javac -d build "%~dp0\SOP-Solver\src\validSolution\Simple.java"
 javac -d build -cp build "%~dp0\SOP-Solver\src\tryAll\Permutations.java"
 javac -d build "%~dp0\SOP-Solver\src\tryAll\recursiveBruteForce.java"
+javac -d build -cp build "%~dp0\SOP-Solver\src\simulatedAnnealing\sa.java"
 
 javac -d build -cp build "%~dp0\SOP-Solver\src\execution\Exe.java"
--- compile.sh
+++ compile.sh
@@ -5,5 +5,6 @@ javac -d build $PWD/SOP-Solver/src/validSolution/OneSolution.java
 javac -d build $PWD/SOP-Solver/src/validSolution/Simple.java
 javac -d build -cp build $PWD/SOP-Solver/src/tryAll/Permutations.java
 javac -d build $PWD/SOP-Solver/src/tryAll/recursiveBruteForce.java
+javac -d build -cp build $PWD/SOP-Solver/src/simulatedAnnealing/sa.java
 
 javac -d build -cp build $PWD/SOP-Solver/src/execution/Exe.java
