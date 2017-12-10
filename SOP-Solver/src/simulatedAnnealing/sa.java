89b0bbc092c315c027b3de8529a4ca2a7995c495
dalu2104 <stu124145@mail.uni-kiel.de>
2017-12-10 16:52:05 +0100

    Fixed algorithm, it now produces a correct input. User has option to choose important parameters.

--- SOP-Solver/src/simulatedAnnealing/sa.java
+++ SOP-Solver/src/simulatedAnnealing/sa.java
@@ -1,8 +1,16 @@
 package simulatedAnnealing;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+import tryAll.Permutations;
+import tryAll.recursiveBruteForce;
+import validSolution.GreedySOP;
+import validSolution.OneSolution;
 import validSolution.Simple;
 
 /**
@@ -14,32 +22,111 @@ import validSolution.Simple;
  *
  */
 public class sa {
+	private static int[][] A;
+	private static Random generator = new Random();
 
 	/**
-	 * Returns a optimal solution as a list according to the given restrictions
-	 * inside the matrix.
+	 * Returns optimal solution for given matrix and asks the user for his input
 	 * 
 	 * @param matrix
-	 *            Given matrix that contains the distances and restrictions.
+	 *            Given matrix for the SOP problem.
+	 * @return A valid good solution.
+	 * @throws IOException 
+	 * @throws NumberFormatException 
+	 */
+	public static List<Integer> simulatedAnnealing(int[][] matrix) throws NumberFormatException, IOException {
+		A = matrix;
+		List<Integer> returner = null;
+
+		// input prep
+		InputStreamReader in = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(in);
+		System.out.println("Please choose by entering a number:");
+		System.out.println("1 - Default parameters.");
+		System.out.println("2 - Choice of parameters.");
+
+		int n = Integer.parseInt(br.readLine());
+
+		// Executing algorithm according to user and calculating execution
+		// time.
+		switch (n) {
+		case 1:
+			returner = simulatedAnnealing1();
+			break;
+		case 2:
+			//Choosing parameters
+			// Temperature
+			System.out.println("Enter temperature.");
+			double temp = Double.parseDouble(br.readLine());
+			
+			// temperature decrement
+			System.out.println("Enter temperature decrementation.");
+			double tempDecr = Double.parseDouble(br.readLine());
+			
+			// iterations
+			System.out.println("Enter amount of iterations.");
+			int itera = Integer.parseInt(br.readLine());
+			
+			returner = simulatedAnnealing2(temp, tempDecr, itera);
+			break;
+		default:
+			System.out.println("Invalid input.");
+			return returner;
+		}
+		return returner;
+	}
+
+	/**
+	 * Default mode, no user input for parameters. Returns a optimal solution as
+	 * a list according to the given restrictions inside the matrix.
+	 * 
 	 * @return An optimal tour as List of Integers corresponding to the vertices
 	 *         in the matrix.
 	 */
-	public static List<Integer> simulatedAnnealing1(int[][] matrix) {
+	private static List<Integer> simulatedAnnealing1() {
 		// in Order to start, we need a valid solution
-		// BEWARE S DOES NOT CONTAIN START AND END VERTEX. INDIZES ARE
-		// INCREMENTED TO MATCH LOGICAL DESCRIPTION OF SOP !!!
-		List<Integer> s0 = Simple.firstIdea(matrix);
+		// BEWARE S DOES NOT CONTAIN START AND END VERTEX.
+		// getting a valid solution for start.
+		List<Integer> s0 = Simple.firstIdea(A);
 		s0 = fixIndexStart(s0);
-		Random generator = new Random();
 		List<Integer> s1 = null;
-		double T = 100;
-		int kmax = 105;
+		double T = 150;
+		int kmax = 250;
 
 		for (int k = 0; k < kmax; k++) {
-			T = temperature(T, true);
-			s1 = randomNeighbour(s0, matrix, generator);
-			if (P(cost(s0, matrix), cost(s1, matrix), T) >= generator.nextDouble()) {
-				s0 = s1;
+			T = temperature(T, true, 0);
+			s1 = randomNeighbour(s0);
+			if (P(cost(s0), cost(s1), T) >= generator.nextDouble()) {
+				s0 = copyList(s1);
+			}
+		}
+
+		s0 = fixIndexEnd(s0);
+		return s0;
+	}
+
+	/**
+	 * Called with user input parameters. Returns a optimal solution as a list
+	 * according to the given restrictions inside the matrix.
+	 * 
+	 * @return An optimal tour as List of Integers corresponding to the vertices
+	 *         in the matrix.
+	 */
+	private static List<Integer> simulatedAnnealing2(double userTemp, double userTempDecr, int userMaxIt) {
+		// in Order to start, we need a valid solution
+		// BEWARE S DOES NOT CONTAIN START AND END VERTEX.
+		// getting a valid solution for start.
+		List<Integer> s0 = Simple.firstIdea(A);
+		s0 = fixIndexStart(s0);
+		List<Integer> s1 = null;
+		double T = 150;
+		int kmax = 250;
+
+		for (int k = 0; k < kmax; k++) {
+			T = temperature(T, true, 0);
+			s1 = randomNeighbour(s0);
+			if (P(cost(s0), cost(s1), T) >= generator.nextDouble()) {
+				s0 = copyList(s1);
 			}
 		}
 
@@ -48,7 +135,7 @@ public class sa {
 	}
 
 	/*
-	 * ____________HELPING METHODS (SA RELATED____________________________
+	 * ____________HELPING METHODS (SA RELATED)____________________________
 	 */
 	/**
 	 * Returns probability to switch to given solution S1, depending on the
@@ -63,14 +150,14 @@ public class sa {
 	 * @return The probability.
 	 */
 	private static double P(int costs0, int costs1, double T) {
-		if(T == 0 && costs1<costs0){
+		if (T == 0 && costs1 < costs0) {
 			return 1;
-		}else if(T == 0){
+		} else if (T == 0) {
 			return 0;
-		}else if(costs1< costs0){
+		} else if (costs1 < costs0) {
 			return 1;
 		} else {
-			return Math.exp(-((costs1 - costs0)/T));
+			return Math.exp(-((costs1 - costs0) / T));
 		}
 	}
 
@@ -81,13 +168,12 @@ public class sa {
 	 * @param defaultMode
 	 * @return new temperature
 	 */
-	private static double temperature(double T, boolean defaultMode) {
+	private static double temperature(double T, boolean defaultMode, int step) {
 		if (defaultMode) {
 			T--;
 		} else {
-			// TODO
+			T -= step;
 		}
-
 		return T;
 	}
 
@@ -100,27 +186,88 @@ public class sa {
 	 * @param matrix
 	 * @return A valid neighbor of the given list.
 	 */
-	private static List<Integer> randomNeighbour(List<Integer> s0, int[][] matrix, Random generator) {
+	private static List<Integer> randomNeighbour(List<Integer> s0) {
 		// copy to safe the original
-		List<Integer> original = new ArrayList<Integer>(); 
-		for(int i: s0){
-			original.add(i);
-		}
+		List<Integer> original = new ArrayList<Integer>();
+		original = copyList(s0);
 
-		
-		int r = generator.nextInt(s0.size());
-		s0 = switchVertex(r, s0);
-
-		if (isValid(s0, matrix) == false) {
-			randomNeighbour
+		while (true) {
+			s0 = copyList(original);
+			int r = generator.nextInt(s0.size());
+			s0 = switchVertex(r, s0);
+			// Made a valid change, breaking out of loop.
+			if (isValid(s0)) {
+				break;
+			}
 		}
 		return s0;
+	}
 
+	// Include this method in SA related due to its randomness.
+	/**
+	 * Switches the vertex at the index r in the list s0 with a random neighbor.
+	 * 
+	 * @param r
+	 *            Index in the list.
+	 * @param s0
+	 *            List of vertices to switch.
+	 */
+	private static List<Integer> switchVertex(int r, List<Integer> s0) {
+		// first case, r is the last index, can only switch with the lower
+		// neighbor
+		if (r == s0.size() - 1) {
+			s0 = switchLower(s0, r);
+			// r is index 0, can only switch with the upper neighbor.
+		} else if (r == 0) {
+			s0 = switchUpper(s0, r);
+			// r is somewhere in the middle, switch with a random neighbor.
+		} else {
+			// int ud = 0 for lower neighbor, ud = 1 for upper.
+			int ud = generator.nextInt(2);
+			if (ud == 1) {
+				s0 = switchUpper(s0, r);
+			} else {
+				s0 = switchLower(s0, r);
+			}
+		}
+		return s0;
 	}
 
 	/*
 	 * ____________HELPING METHODS (NON SA RELATED)_________________________
 	 */
+
+	/**
+	 * Switches vertex at the index in the list with its upper neighbor.
+	 * 
+	 * @param s0
+	 *            Given list.
+	 * @param r
+	 *            Given index.
+	 * @return The list with the switched vertices.
+	 */
+	private static List<Integer> switchUpper(List<Integer> s0, int r) {
+		int temp = s0.get(r + 1);
+		s0.set(r + 1, s0.get(r));
+		s0.set(r, temp);
+		return s0;
+	}
+
+	/**
+	 * Switches vertex at the index in the list with its upper neighbor.
+	 * 
+	 * @param s0
+	 *            Given list.
+	 * @param r
+	 *            Given index.
+	 * @return The list witht the switched vertices.
+	 */
+	private static List<Integer> switchLower(List<Integer> s0, int r) {
+		int temp = s0.get(r - 1);
+		s0.set(r - 1, s0.get(r));
+		s0.set(r, temp);
+		return s0;
+	}
 
 	/**
 	 * A method checking if a tour is valid.
@@ -133,11 +280,13 @@ public class sa {
 	 *            visited before node i.
 	 * @return True if it the tour is valid, false if not.
 	 */
-	private static boolean isValid(List<Integer> newSolution, int[][] A) {
-		for (int i = 1; i < newSolution.size() - 1; i++) {
-			// check if another one has to be visited before
-			if (A[newSolution.get(i - 1)][newSolution.get(i)] == -1)
-				return false;
+	private static boolean isValid(List<Integer> newSolution) {
+		for (int i = 1; i < newSolution.size(); i++) {
+			for (int j = 0; j < i; j++) {
+				// check if another one has to be visited before
+				if (A[newSolution.get(j)][newSolution.get(i)] == -1)
+					return false;
+			}
 		}
 		return true;
 	}
@@ -153,9 +302,9 @@ public class sa {
 	 *            visited before node i.
 	 * @return The costs of the tour
 	 */
-	private static int cost(List<Integer> tour, int[][] A) {
+	private static int cost(List<Integer> tour) {
 		int distance = 0;
-		for (int i = 0; i < tour.size(); i++) {
+		for (int i = 0; i < tour.size() - 1; i++) {
 			distance += A[tour.get(i)][tour.get(i + 1)];
 		}
 		return distance;
@@ -183,5 +332,20 @@ public class sa {
 			s0.set(i, temp);
 		}
 		return s0;
+	}
+
+	/**
+	 * Method that copies the contents in a list to another new list.
+	 * 
+	 * @param from
+	 *            Contents come from this list.
+	 * @return The new copy of the list.
+	 */
+	private static List<Integer> copyList(List<Integer> from) {
+		List<Integer> to = new ArrayList<Integer>();
+		for (int i : from) {
+			to.add(i);
+		}
+		return to;
 	}
 }
