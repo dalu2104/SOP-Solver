059b187c1980239dd26a8b1b026ad3d55e9f169a
dalu2104 <stu124145@mail.uni-kiel.de>
2017-12-18 13:47:09 +0100

    Fixes syntax issue.

--- SOP-Solver.sh
+++ SOP-Solver.sh
@@ -2,9 +2,10 @@
 echo Willkommen beim SOP-Solver.
 echo Waehlen Sie bitte eine Testinstanz aus dem Ordner 'SOP-Instanzen':
 while(true)
-	do read -p "Dateiname inkl. Dateiendung, zB esc07.sop:" dateiname
-	do java -cp build execution.Exe $PWD/SOP-Instanzen/$dateiname
-	do read -p "Neue Datei probieren? y/n" newFile
+do 	
+	read -p "Dateiname inkl. Dateiendung, zB esc07.sop:" dateiname
+	java -cp build execution.Exe $PWD/SOP-Instanzen/$dateiname
+	read -p "Neue Datei probieren? y/n" newFile
 	if [ $newFile != "y"]
 	then
 		break
