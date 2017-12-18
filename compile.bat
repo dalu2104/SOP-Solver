0ff1722056fe4996044e64295e5240db1b7f25a6
dalu2104 <stu124145@mail.uni-kiel.de>
2017-12-18 10:48:29 +0100

    Updated build scripts according to new algorithm.

--- compile.bat
+++ compile.bat
@@ -1,13 +1,20 @@
 mkdir build
 javac -d build "%~dp0\SOP-Solver\src\convertSOPFileToArray\parser.java"
 javac -d build "%~dp0\SOP-Solver\src\execution\TimeStartAndStop.java"
-javac -d build "%~dp0\SOP-Solver\src\simulatedAnnealing\ExecutionTimeAndSolution.java"
 
 javac -d build "%~dp0\SOP-Solver\src\validSolution\GreedySOP.java"
 javac -d build "%~dp0\SOP-Solver\src\validSolution\OneSolution.java"
 javac -d build "%~dp0\SOP-Solver\src\validSolution\Simple.java"
+
 javac -d build -cp build "%~dp0\SOP-Solver\src\tryAll\Permutations.java"
 javac -d build "%~dp0\SOP-Solver\src\tryAll\recursiveBruteForce.java"
+
+javac -d build "%~dp0\SOP-Solver\src\simulatedAnnealing\ExecutionTimeAndSolution.java"
 javac -d build -cp build "%~dp0\SOP-Solver\src\simulatedAnnealing\sa.java"
 
+javac -d build -cp build "%~dp0\SOP-Solver\src\genetic\UsefulMethods.java"
+javac -d build -cp build "%~dp0\SOP-Solver\src\genetic\GenAlg.java"
+javac -d build -cp build "%~dp0\SOP-Solver\src\genetic\StartGenAlg.java"
+
 javac -d build -cp build "%~dp0\SOP-Solver\src\execution\Exe.java"
+PAUSE
