9fce68b4f3ca6b3b29c17fd3e9b8905c81e132d6
dalu2104 <stu124145@mail.uni-kiel.de>
2017-12-18 14:53:23 +0100

    Fixed bug.

--- SOP-Solver.bat
+++ SOP-Solver.bat
@@ -5,6 +5,6 @@ echo Waehlen Sie bitte eine Testinstanz aus dem Ordner 'SOP-Instanzen':
 set /p dateiname=Dateiname inkl. Dateiendung, zB esc07.sop:
 java -cp build execution.Exe "%~dp0\SOP-Instanzen\%dateiname%"
 set /p neueDatei=Neue Datei ausfuehren? y/n
-if /i %neueDatei%=="y" goto FILE
+if /i "%neueDatei%" EQU "y" goto :FILE
 echo Zum Beenden:
 PAUSE
