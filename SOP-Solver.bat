984fc41538105c1bbc426d8bff87bb591be8cb7f
Daniel Lucas <stu124145@mail.uni-kiel.de>
2018-01-12 12:52:56 +0100

    Minor change.

--- SOP-Solver.bat
+++ SOP-Solver.bat
@@ -4,7 +4,8 @@ echo Willkommen beim SOP-Solver.
 echo Waehlen Sie bitte eine Testinstanz aus dem Ordner 'SOP-Instanzen':
 set /p dateiname=Dateiname inkl. Dateiendung, zB esc07.sop:
 java -cp build execution.Exe "%~dp0\SOP-Instanzen\%dateiname%"
-set /p neueDatei=Neue Datei ausfuehren? y/n
+echo Neue Datei ausfuehren? y/n
+set /p neueDatei=
 if /i "%neueDatei%" EQU "y" goto :FILE
 echo Zum Beenden:
 PAUSE
